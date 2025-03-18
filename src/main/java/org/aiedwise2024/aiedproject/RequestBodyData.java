4c9255b88fa6a88aa28303b11a09ef21ac17911a
Meg Horikawa <meg.horikawa@gmail.com>
2025-03-18 14:01:23 +0100

    responseBodyData debug

--- src/main/java/org/aiedwise2024/aiedproject/RequestBodyData.java
+++ src/main/java/org/aiedwise2024/aiedproject/RequestBodyData.java
@@ -1,17 +1,23 @@
 package org.aiedwise2024.aiedproject;
 
 import java.util.List;
+import java.util.Map;
 
 /**This class handles the parameters to be sent to the model for prompting
  * we are using groq's API call format here with documentation found here
  * https://console.groq.com/docs/api-reference#chat
+ *
+ * response format must be sent as:
+ * "response_format": {"type": "json_object"}
+ *
  */
-
 
 public class RequestBodyData {
     String model = "deepseek-r1-distill-llama-70b";
-    double temp = 0.0; //higher temperature introduces more randomness ares usese 0.0
-    String responseFormat = "{ \"response_format\": \"json_object\" }";
+    double temperature = 0.0; //higher temperature introduces more randomness ares uses 0.0
+
+    //response_format
+    Map<String, String> response_format = Map.of("type", "json_object");
 
     List<LMmessage> messages; // list of messages to include to model
 
@@ -30,19 +36,15 @@ public class RequestBodyData {
     }
 
     public void setTemp(double temp) {
-        this.temp = temp;
+        this.temperature = temp;
     }
 
     public double getTemp() {
-        return temp;
+        return temperature;
     }
 
-    public void setResponseFormat(String responseFormat) {
-        this.responseFormat = responseFormat;
-    }
-
-    public String getResponseFormat() {
-        return responseFormat;
+    public Map<String,String> getResponseFormat() {
+        return response_format;
     }
 
     public List<LMmessage> getMessages() {
